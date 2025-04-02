7652d2c67dd0ce2ec0452ff7f415f133a5390b59
Wolyaon <jarrick_pang@hotmail.com>
2025-04-01 20:46:07 -0700

    Updated convertExcel to create json to replicate a database with Id numbers.

--- src/convertExcel.py
+++ src/convertExcel.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import json
 
 def cleanDF(df: pd.DataFrame) -> pd.DataFrame:
-    labels = ["name", "pic", "sizes", "price", "tested", "finalized", "description", "Notes"]
+    labels = ["name", "pic", "sizes", "price", "tested", "finalized", "description", "Notes", "type"]
     df.columns = labels
     # We dont need the picture or notes as those are internal usage only
     df = df.drop(["pic", "Notes"], axis=1)
@@ -16,7 +17,6 @@ def cleanDF(df: pd.DataFrame) -> pd.DataFrame:
     df["price"].fillna("No price set yet", inplace=True)
     df["tested"] = df["tested"].astype(bool)
     df["finalized"] = df["finalized"].astype(bool)
-    print(df.dtypes)
     return df                                                                       
 
 def changeIndexToName(df: pd.DataFrame) -> pd.DataFrame:
@@ -25,23 +25,30 @@ def changeIndexToName(df: pd.DataFrame) -> pd.DataFrame:
 
 
 xcel = pd.ExcelFile("Menu.xlsx")
-cakeDF = pd.read_excel("Menu.xlsx", "Cakes")
-tartDF = pd.read_excel("Menu.xlsx", "Tarts")
-otherDF = pd.read_excel("Menu.xlsx", "Other")
+cakeDF = pd.read_excel("Menu.xlsx", "Cakes").assign(type="cake")
+tartDF = pd.read_excel("Menu.xlsx", "Tarts").assign(type="tart")
+otherDF = pd.read_excel("Menu.xlsx", "Other").assign(type="other")
 
 cakeDF = cleanDF(cakeDF)
 tartDF = cleanDF(tartDF)
 otherDF = cleanDF(otherDF)
 
-indCakeDF = changeIndexToName(cakeDF)
-indTartDF = changeIndexToName(tartDF)
-inOtherDF = changeIndexToName(otherDF)
+# Need to create a DF with the entire cake list as we will use
+# this for grabbing cakes by their IDs instead of using a DB
+totDF = pd.concat([cakeDF, tartDF, otherDF]).reset_index(drop=True)
 
-totDF = pd.concat([indCakeDF, indTartDF, inOtherDF])
+# Using the index numbers we give each cake a unique ID number
+totDF["productId"] = totDF.index.astype(str)
+#print(totDF)
 totDF.to_json("AllCakes.json", orient="index")
 
 # We want to print out the json in each line as an individual object
 # so we orient by records.
-cakeDF.to_json("cakeInfo.json", orient="records")
-tartDF.to_json("tartInfo.json", orient="records")
-otherDF.to_json("otherInfo.json", orient="records")
+totDF.loc[totDF["type"]=="cake"].to_json("cakeInfo.json", orient="records")
+totDF.loc[totDF["type"]=="tart"].to_json("tartInfo.json", orient="records")
+totDF.loc[totDF["type"]=="other"].to_json("otherInfo.json", orient="records")
+
+CAKES = []
+with open("AllCakes.json") as file:
+    CAKES = json.load(file)
+print(CAKES["2"])
